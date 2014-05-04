268adffc5e602a2fdb5f38565a66074ee591df44
kevinkang88 <kevinkang88@gmail.com>
2014-05-04 08:11:23 -0700

    add solution to sqlite

--- week_8_and_9/5_SQL/SQLite_gem/my_solution.rb
+++ week_8_and_9/5_SQL/SQLite_gem/my_solution.rb
@@ -8,20 +8,31 @@ require 'sqlite3'
 $db = SQLite3::Database.open "congress_poll_results.db"
 
 
-def print_arizona_reps
-  puts "AZ REPRESENTATIVES"
-  az_reps = $db.execute("SELECT name FROM congress_members WHERE location = 'AZ'")
-  az_reps.each { |rep| puts rep }
+def print_arizona_reps(state)
+  puts "#{state} REPRESENTATIVES"
+  reps = $db.execute("SELECT name FROM congress_members WHERE location = '#{state.to_s}'")
+  reps.each { |rep| puts rep }
 end
 
 def print_longest_serving_reps(minimum_years)  #sorry guys, oracle needs me, i didn't finish this!
   puts "LONGEST SERVING REPRESENTATIVES"
-  puts $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  name = $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  year = $db.execute("SELECT years_in_congress FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  
+  name_year = name.zip(year)
+  name_year.each {|name, year| puts "#{name} - #{year} years"}
 end
 
-def print_lowest_grade_level_speakers
-  puts "LOWEST GRADE LEVEL SPEAKERS (less than < 8th grade)"
+def print_lowest_grade_level_speakers(grade_level)
+  puts "LOWEST GRADE LEVEL SPEAKERS (less than < #{grade_level})"
+  puts $db.execute("SELECT name, grade_current FROM congress_members WHERE grade_current < #{grade_level}")
 end
+
+def print_votes_by_reps 
+  puts "AMOUNTS OF VOTES PER REPRESENTATIVE"
+  puts $db.execute("SELECT name, count(politician_id) AS votes FROM congress_members JOIN votes ON (congress_members.id = politician_id) GROUP BY name")
+end
+
 
 def print_separator
   puts 
@@ -29,8 +40,18 @@ def print_separator
   puts 
 end
 
+#years_in_congress
+print_arizona_reps("AZ")
+print_separator
+print_arizona_reps("NY")
+print_separator
+print_arizona_reps("AK")
+print_separator
+print_arizona_reps("NJ")
+print_separator
+print_arizona_reps("FL")
 
-print_arizona_reps
+
 
 print_separator
 
@@ -39,7 +60,10 @@ print_longest_serving_reps(35)
 # output should look like:  Rep. C. W. Bill Young - 41 years
 
 print_separator
-print_lowest_grade_level_speakers 
+print_lowest_grade_level_speakers(8)
+
+print_separator
+print_votes_by_reps
 # TODO - Need to be able to pass the grade level as an argument, look in schema for "grade_current" column
 
 # TODO - Make a method to print the following states representatives as well:
@@ -55,7 +79,22 @@ print_lowest_grade_level_speakers
 #    have a similar format)
 # Create a listing of each Politician and the voter that voted for them
 # output should include the senators name, then a long list of voters separated by a comma
-#
+
+def voter_per_member
+  row_ex = $db.execute("SELECT politician_id, voter_id FROM votes")
+  
+  politicians = []
+  voter = []
+  row_ex.each do
+    politicians <<  $db.execute("SELECT name FROM congress_members WHERE id = '#{row_ex[0]}'")
+    voter <<  $db.execute("SELECT first_name FROM congress_members WHERE id = '#{row_ex[1]}'")
+  end
+  puts politicians.zip(voter)
+
+end
+
+voter_per_member
+
 # * you'll need to do some join statements to complete these last queries!
 
 
